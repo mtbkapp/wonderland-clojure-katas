e4716a76634c9a8d229bee60e9401910f90708af
Jason Kapp <jkapp@rallydev.com>
2017-01-11 09:27:48 -0700

    solution to wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
-(defn wonderland-number []
-  ;; calculate me
-  42)
+(defn same-digits?
+  [x y]
+  (= (set (str x))
+     (set (str y))))
+
+(defn wonderland-number 
+  []
+  (some (fn [x]
+          (if (and (same-digits? x (* 2 x))
+                   (same-digits? x (* 3 x))
+                   (same-digits? x (* 4 x))
+                   (same-digits? x (* 5 x))
+                   (same-digits? x (* 6 x)))
+            x))
+    (range 10000 (inc (quot 999999 6)))))
