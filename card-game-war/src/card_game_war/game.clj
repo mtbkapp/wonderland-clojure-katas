c8aa452e9e1f57457e929c4d00ec8d2d0efb987e
Jason Kapp <jkapp@rallydev.com>
2019-03-07 16:45:08 -0700

    mas

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,43 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn vector->rank-map
+  [v]
+  (zipmap v (range 0 (count v))))
 
-(defn play-game [player1-cards player2-cards])
+(def suits-rank-map (vector->rank-map suits))
+(def ranks-rank-map (vector->rank-map ranks))
+
+(defn card-compare
+  [[suit-x rank-x :as card-x] [suit-y rank-y :as card-y]]
+  (let [r (compare (get ranks-rank-map rank-x)
+                   (get ranks-rank-map rank-y))]
+    (if-not (zero? r) 
+      r
+      (compare (get suits-rank-map suit-x)
+               (get suits-rank-map suit-y)))))
+
+(defn play-round 
+  [[[p1-card & player1-cards] [p2-card % player2-cards]]]
+  (let [c (card-compare p1-card p2-card)]
+    (cond (neg? c)
+          [player1-cards (conj player2-cards p1-card p2-card)]
+          (pos? c)
+          [(conj player1-cards p1-card p2-card) player2-cards]
+          :else
+          [(conj player1-cards p1-card) (conj player2-cards p2-card)])))
+
+(defn play-game
+  [game]
+  
+  )
+
+(defn game
+  [] 
+  (let [cards (shuffle cards)]
+    (->> cards 
+         (partition (/ (count cards) 2))
+         (map vec)
+         (into []))))
+
+
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -3,16 +3,18 @@
             [card-game-war.game :refer :all]))
 
 
+(deftest test-card-compare
+  (let [sorted-cards (sort card-compare (shuffle cards))]
+    (is (= (map first sorted-cards)
+           (take 52 (cycle suits))))
+    (is (= (map second sorted-cards)
+           (take 52 (flatten (map (partial repeat 4) 
+                                  (cycle ranks))))))))
+
 ;; fill in  tests for your game
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+
+  )
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
